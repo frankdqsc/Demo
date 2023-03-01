6d0492ec34ddf4754f86fa1942a883f348cc5d23
frankdqsc <frankdqsc@gmail.com>
2023-03-01 23:40:47 +0800

    1

--- API/Object/RewriteObjectClass/Student.java
+++ API/Object/RewriteObjectClass/Student.java
@@ -1,5 +1,7 @@
 package API.Object.RewriteObjectClass;
 
+import java.util.Objects;
+
 public class Student {
     //成员变量
     private int id;
@@ -47,6 +49,24 @@ public class Student {
                 '}';
     }
 
-    //equals()
+    //equals() 用来比较2个对象是否相同，自定义的重写，假设id相同，2个对象就是同一个对象
+/*    public boolean equals(Object obj){ // s1.equals(s2)
 
+*//*      s2赋值给父类 object 后就向上转型-多态（父类的引用不能使用子类的特有成员）
+        return this.id == obj.id; *//*
+        //向下转型
+        Student s2 = (Student)obj;
+        return this.id == s2.id;
+    }*/
+
+    //自动生成 equals
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass()) //same class judge
+            return false;
+        Student student = (Student) o;
+        return id == student.id && score == student.score && name.equals(student.name);
+    }
 }
--- API/Object/RewriteObjectClass/Test.java
+++ API/Object/RewriteObjectClass/Test.java
@@ -8,10 +8,18 @@ package API.Object.RewriteObjectClass;
 public class Test {
     public static void main(String[] args) {
         Student s1 = new Student(1,"Frank",70);
+
 /*      默认使用了该对象的 toString() 方法，在不重写 Object 中 toString 时，使用默认
         不重写时：API.Object.RewriteObjectClass.Student@1b6d3586
         重写时：Student{id=1, name='Frank', score=70} */
+
         System.out.println(s1);
-        System.out.println(s1.toString());
+        //System.out.println(s1.toString());
+
+        //测试 equals 方法
+        Student s2 = new Student(1, "Tim",6);
+        boolean b1 = s1.equals(s2);
+        System.out.println(b1);
+
     }
 }
